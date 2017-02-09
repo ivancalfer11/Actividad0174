0504f53cf502cb67e327c1e69adedb768aa5b291
ivanozil11 <ivancalfer1@gmail.com>
2017-02-09 10:23:41 +0100

    mi primer commit

---
+++ Comida.java
@@ -0,0 +1,36 @@
+
+/**
+ * Write a description of class Comida here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Comida
+{
+    // instance variables - replace the example below with your own
+    private String nombreComida;
+    
+    private int cantidadCalorias;
+
+    /**
+     * Constructor for objects of class Comida
+     */
+    public Comida(String nombreComida , int cantidadCalorias)
+    {
+        this.nombreComida = nombreComida;
+        this.cantidadCalorias = cantidadCalorias;
+      
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public int getCantidadCalorias()
+    {
+        return cantidadCalorias;
+        
+    }
+}
---
+++ Persona.java
@@ -0,0 +1,43 @@
+
+/**
+ * Write a description of class Persona here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Persona
+{
+    // instance variables - replace the example below with your own
+    private String nombrePersona;
+    private boolean hombre;
+    private int pesoKilos;
+    private int alturaCm;
+    private int edad;
+    private int caloriasIngeridas;
+    
+
+    /**
+     * Constructor for objects of class Persona
+     */
+    public Persona(String nombrePersona , boolean hombre, int pesoKilos , int alturaCm , int edad)
+    {
+        this.nombrePersona= nombrePersona;
+        this.hombre = hombre;
+        this.pesoKilos = pesoKilos;
+        this.alturaCm = alturaCm;
+        this.edad = edad;
+        caloriasIngeridas = 0;
+        
+        
+    }
+    public int getCaloriasIngeridas()
+    {
+        return caloriasIngeridas;
+       
+    }
+    public int comer(Comida newComida){
+        
+        return caloriasIngeridas;
+        
+    }
+}
